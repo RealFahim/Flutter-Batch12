5f6221852d790fa5ef01020533b395b4f5c39448
Fahim <fahimbd286623@gmail.com>
2025-09-04 15:00:44 +0600

    Form Validation Add and Log in page update

--- lib/Component/my_textfield.dart
+++ lib/Component/my_textfield.dart
@@ -1,24 +1,80 @@
 import 'package:flutter/material.dart';
 
 class MyTextField extends StatelessWidget {
-  const MyTextField({super.key});
+   final TextEditingController controller;
+    final String  hintText;
+    final bool obscureText;
+    final String? Function(String?)? validator;
+
+
+
+  const MyTextField({
+    super.key,
+    required this.hintText,
+    required this.controller,
+    required this.obscureText,
+    this.validator, 
+    //required this.controller
+    
+    });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 25.0),
-      child: TextField(
+      padding: const EdgeInsets.only(top: 25),
+      child: TextFormField(
+        controller: controller,
+        obscureText:obscureText,
+        validator: validator,
         decoration: InputDecoration(
+          hintText: hintText,
           enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.white),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey),
-          ),
-          fillColor: Colors.grey,
+           borderSide: BorderSide(color: Colors.white70),
+           ),  
+           focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+           ),
+          fillColor: Colors.grey[500],
           filled: true,
+          hintStyle: TextStyle(color: Colors.grey[600]),
         ),
       ),
     );
   }
 }
+
+
+// class MyTextField extends StatelessWidget {
+//   final TextEditingController controller;
+//   final String hintText;
+//   final bool obscureText;
+//    MyTextField({
+//      super.key,
+//      required this.controller,
+//      required this.hintText,
+//      required this.obscureText,
+//    });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: EdgeInsets.symmetric(horizontal: 25.0),
+//       child: TextField(
+//         controller: controller,
+//         obscureText: obscureText,
+//         decoration: InputDecoration(
+//           enabledBorder: OutlineInputBorder(
+//             borderSide: BorderSide(color: Colors.white70),
+//           ),
+//           focusedBorder: OutlineInputBorder(
+//             borderSide: BorderSide(color: Colors.black),
+//           ),
+//           fillColor: Colors.grey[500],
+//           filled: true,
+//           hintText: hintText,
+//           hintStyle: TextStyle(color: Colors.grey[600]),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,41 +1,179 @@
 import 'package:flutter/material.dart';
 import 'package:login_ui/Component/my_textfield.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class Loginpage extends StatelessWidget {
+  Loginpage({super.key});
+  final _formKey = GlobalKey<FormState>();
+  final emailcontroller = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: const [
-              SizedBox(height: 50),
+      appBar: AppBar(title: Text("Login Page Validation")),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Form(
+          key: _formKey,
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 50),
 
-              Icon(Icons.lock, size: 100),
-
-              SizedBox(height: 50),
-
+              Icon(Icons.lock, size: 90),
+               SizedBox(height: 70),
               Text(
                 "Welcome Back you\"ve been missed",
-                style: TextStyle(color: Colors.grey, fontSize: 16),
+                style: TextStyle(color: Colors.grey[600], fontSize: 16),
               ),
 
-            SizedBox(height: 50),
-            //User name text field
-            MyTextField(),
-             SizedBox(height: 10),
-            //password textfield
-            MyTextField(),
+              SizedBox(height: 20),
 
-          
+                MyTextField(
+                  controller: emailcontroller,
+                  hintText: 'আপনার Email লিখুন',
+                  obscureText: false,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'অনুগ্রহ করে ইমেল লিখুন';
+                    }
+                    return null;
+                  },
+                
+                ),
+                SizedBox(height: 5),
+                MyTextField(
+                  controller: passwordController,
+                  hintText: 'আপনার Password লিখুন',
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'অনুগ্রহ করে পাসওয়ার্ড লিখুন';
+                    }
+                    if (value.length < 6) {
+                      return 'পাসওয়ার্ডটি কমপক্ষে ৬ অক্ষরের হতে হবে';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 10),
 
-            ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                child: Row(
+                  mainAxisAlignment:MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Forgot Password",
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                  ],
+                ),
+              ),
+                SizedBox(height: 25),
+                ElevatedButton(
+                  onPressed: () {
+                     // এখানে ভ্যালিডেশন চালু করা হচ্ছে
+                    if (_formKey.currentState!.validate()) {
+                    // যদি ভ্যালিডেশন সফল হয়, তবে এখানে লগইন লজিক লিখুন।
+                    print('ইমেইল: ${emailcontroller.text}');
+                  print('পাসওয়ার্ড: ${passwordController.text}');
+                }
+                  },
+                  child: Text("Login"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:login_ui/Component/my_textfield.dart';
+
+// class LoginPage extends StatelessWidget {
+//   LoginPage({super.key});
+
+//   final TextEditingController usernameController = TextEditingController();
+//   final TextEditingController passwordController = TextEditingController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.grey[300],
+//       body: SafeArea(
+//         child: Center(
+//           child: Column(
+//             children: [
+
+//               SizedBox(height: 50),
+
+//               Icon(Icons.lock, size: 90),
+
+//               SizedBox(height: 70),
+
+//               Text(
+//                 "Welcome Back you\"ve been missed",
+//                 style: TextStyle(color: Colors.grey[600], fontSize: 16),
+//               ),
+
+//               SizedBox(height: 20),
+
+//               //User name text field
+//               MyTextField(
+//                 controller: usernameController,
+//                 hintText: "Username",
+//                 obscureText: false,
+//               ),
+
+//               SizedBox(height: 5),
+
+//               //password textfield
+//               MyTextField(
+//                 controller: passwordController,
+//                 hintText: "Password",
+//                 obscureText: true,
+//               ),
+
+//               SizedBox(height: 10),
+
+//               Padding(
+//                 padding: const EdgeInsets.symmetric(horizontal: 25),
+//                 child: Row(
+//                   mainAxisAlignment:MainAxisAlignment.end,
+//                   children: [
+//                     Text(
+//                       "Forgot Password",
+//                       style: TextStyle(color: Colors.grey[700]),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+
+//               SizedBox(height: 10),
+
+//               ElevatedButton(
+//               style: ElevatedButton.styleFrom(
+                
+//               ),
+//                 onPressed: () {
+//                   print("Username: ${usernameController.text}");
+//                   print("Password: ${passwordController.text}");
+//                 },
+//                 child: Text("Login"),
+//               ),
+
+              
+
+
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
